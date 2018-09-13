061353b63980e81b6f07ee2854beab58039679d9
CASV <casv@MBP-de-Usuario.home>
2018-09-13 20:53:38 -0300

    modificaiones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,36 +5,26 @@ import { NgModule } from '@angular/core';
 
 import { APP_ROUTE } from './app.route';
 
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
-  ],
+   ],
   imports: [
     BrowserModule,
-    APP_ROUTE
+    APP_ROUTE,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
